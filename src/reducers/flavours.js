f3a0dc576686334e7464a01963c1d9879150d849
Chris Pimenta <pimenta.chrisj2@gmail.com>
2018-01-11 19:34:05 +0100

    Reducer function for incrementing likes and changed title icon

--- src/reducers/flavours.js
+++ src/reducers/flavours.js
@@ -1,5 +1,15 @@
 function flavours(state = [], action) {
-  return state;
+  switch (action.type) {
+    case 'FLAVOUR::INCREMENT_LIKES':
+      const i = action.index;
+      return [
+        ...state.slice(0, i), //Take everything before the actual element in the state we changing
+        { ...state[i], likes: state[i].likes + 1 }, //Take the actual element we are changing and the increased likes
+        ...state.slice(i + 1) //Take everything after the actual element in the state we changing
+      ]
+    default:
+      return state;
+  }
 }
 
 export default flavours;
